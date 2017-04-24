b85af6f17258c02fe96ec6c5bc15063ca506c685
DarkThunderbolt <forceofdarkthunderbolt@gmail.com>
2017-04-24 16:28:20 +0300

    something broken

--- WebSiteMusicBand/App_Start/RouteConfig.cs
+++ WebSiteMusicBand/App_Start/RouteConfig.cs
@@ -12,18 +12,19 @@ namespace WebSiteMusicBand
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "News", action = "ListLong", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "News", action = "ListLong", id = UrlParameter.Optional }            );
 
-            routes.MapRoute(
-    name: "Members",
-    url: "{controller}/{action}/{id}",
-    defaults: new { controller = "Members", action = "Index", id = UrlParameter.Optional }
-);
+            //routes.MapRoute(
+            //    name: "Members",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Members", action = "Index", id = UrlParameter.Optional }
+            //);
         }
+
     }
 }
